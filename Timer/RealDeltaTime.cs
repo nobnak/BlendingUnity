502c643860ad6432bfebb0f74e12b036aae4880d
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-08-21 18:01:38 +0900

    no message

---
+++ Timer/RealDeltaTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace nobnak.Timer {
+
+	public class RealDeltaTime {
+		private float _prevTime;
+
+		public RealDeltaTime() {
+			this._prevTime = Time.realtimeSinceStartup;
+		}
+
+		public float DeltaTime() {
+			var realTime = Time.realtimeSinceStartup;
+			var dt = realTime - _prevTime;
+			_prevTime = realTime;
+			return dt;
+		}
+	}
+}
