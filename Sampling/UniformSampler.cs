ee671c567e87824ec108076ab8564c0eef013b69
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-09-14 12:06:58 +0900

    no message

--- Sampling/UniformSampler.cs
+++ Sampling/UniformSampler.cs
@@ -10,5 +10,11 @@ namespace nobnak.Sampling {
 				samples[i] = Random.Range(range.x, range.y);
 			return samples;
 		}
+		public static float[] SamplesInv(Vector2 range, int size) {
+			var samples = Samples(range, size);
+			for (var i = 0; i < size; i++)
+				samples[i] = 1f / samples[i];
+			return samples;
+		}
     }
 }
