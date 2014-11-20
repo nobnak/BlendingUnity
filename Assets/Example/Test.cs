b698896a7f57faef965b9641e6b70f84431c6b06
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-11-20 22:45:13 +0900

    Remove files for test from package

---
+++ Assets/Example/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	public Capture[] captures;
+	public RenderTexture[] targets;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targets = new RenderTexture[captures.Length];
+		for (var i = 0; i < captures.Length; i++)
+			targets[i] = captures[i].GetTarget();
+	}
+}
